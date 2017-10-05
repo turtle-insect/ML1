76bc06e9a486776526f67053ff2fcaf8dc5df6f8
turtle-insect <turtle.insect@gmail.com>
2017-10-06 04:58:31 +0900

    First Commit

---
+++ ML1/DataContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML1
+{
+	class DataContext
+	{
+		public uint PlayHour
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(0x250C, 4) / 3600000;
+			}
+			set
+			{
+				uint number = SaveData.Instance().ReadNumber(0x250C, 4) / 60000 % 60;
+				SaveData.Instance().WriteNumber(0x250C, 4, (value * 60 + number) * 60000);
+			}
+		}
+
+		public uint PlayMinute
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(0x250C, 4) / 60000 % 60;
+			}
+			set
+			{
+				uint number = SaveData.Instance().ReadNumber(0x250C, 4) / 3600000;
+				SaveData.Instance().WriteNumber(0x250C, 4, (value + number * 60) * 60000);
+			}
+		}
+		public uint Coin
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(0x50, 4);
+			}
+
+			set
+			{
+				Util.WriteNumber(0x50, 4, value, 0, 999999);
+			}
+		}
+	}
+}

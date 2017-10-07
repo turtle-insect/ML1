d116226a8f2d2524667ee1301281e4d41cce16f9
turtle-insect <turtle.insect@gmail.com>
2017-10-08 01:02:04 +0900

    Add Charactor Param

--- ML1/Charactor.cs
+++ ML1/Charactor.cs
@@ -24,9 +24,101 @@ namespace ML1
 
 			set
 			{
-				SaveData.Instance().WriteNumber(mAddress, 2, value);
+				Util.WriteNumber(mAddress, 2, value, 0, 999);
 			}
 		}
+
+		public uint BP
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 0x02, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 0x02, 2, value, 0, 999);
+			}
+		}
+
+		public uint HPPlus
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 0x04, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 0x04, 2, value, 0, 999);
+			}
+		}
+
+		public uint BPPlus
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 0x08, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 0x08, 2, value, 0, 999);
+			}
+		}
+
+		public uint POWPlus
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 0x0C, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 0x0C, 2, value, 0, 999);
+			}
+		}
+
+		public uint DEFPlus
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 0x10, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 0x10, 2, value, 0, 999);
+			}
+		}
+
+		public uint SPEEDPlus
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 0x14, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 0x14, 2, value, 0, 999);
+			}
+		}
+
+		public uint Exp
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 0x1C, 4);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 0x1C, 4, value, 0, 9999999);
+			}
+		}
+
 
 		private uint mAddress;
 	}
